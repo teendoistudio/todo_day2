ae7d34f458fc400f1d8561766e3c4967a03aa2cb
Bundit Nuntates <silkyland@gmail.com>
2017-07-07 11:40:42 +0700

    Add HomeController

--- database/seeds/UserTypesSeeder.php
+++ database/seeds/UserTypesSeeder.php
@@ -17,13 +17,5 @@ class UserTypesSeeder extends Seeder
 //        ];
 //
 //        \App\UserType::insert($user_type);
-
-        $user_type = new \App\UserType();
-        $user_type->name = "admin";
-        $user_type->save();
-
-        $user_type = new \App\UserType();
-        $user_type->name = "user";
-        $user_type->save();
     }
 }
